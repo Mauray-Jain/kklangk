4419c9f8fc049b92e6a339f7daaf133da835f781
Mauray-Jain <maurayjain@gmail.com>
2024-12-03 23:15:37 +0530

    mane laage se ki vm complete hui gawa; testing voh ke howe

---
+++ src/Chunk.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+
+ops: std.ArrayList(Ops),
+
+pub const Ops = union(enum) {
+    PUSH: isize,
+    DUP,
+    COPYNTH: isize,
+    SWAP,
+    POP,
+    POPN: isize,
+
+    ADD,
+    SUB,
+    MULT,
+    DIV,
+    MOD,
+
+    HEAPSTR,
+    HEAPRET,
+
+    MARK: isize,
+    CALL: isize,
+    JMP: isize,
+    JMPIF0: isize,
+    JMPIFNEG: isize,
+    RETURN,
+    EXIT,
+
+    OUTCHAR,
+    OUTNUM,
+    INCHAR,
+    INNUM,
+
+    pub fn disas(val: @This()) void {
+        const tag = @tagName(val);
+        inline for (std.meta.fields(@This())) |f| {
+            if (std.mem.eql(u8, f.name, tag)) {
+                std.debug.print("{} {s}\n", .{ @field(val, f.name), f.name });
+            }
+        }
+    }
+};
+
+test "tt" {
+    var t = Ops{ .JMP = 10 };
+    _ = &t;
+    t.disas();
+    const v = Ops{ .EXIT = void{} };
+    v.disas();
+}
--- src/Vm.zig
+++ src/Vm.zig
@@ -1,26 +1,42 @@
 const std = @import("std");
+const Chunk = @import("Chunk.zig");
+const Ops = Chunk.Ops;
 
 const Self = @This();
 
-pub const StackErrors = error{InsufficientElements} || std.mem.Allocator.Error;
+const AllocError = std.mem.Allocator.Error;
+pub const StackErrors = error{InsufficientElements} || AllocError;
 pub const HeapError = error{NoSuchElement};
+pub const LabelErrors = error{InvalidLabel} || AllocError;
+pub const CallError = error{EmptyCallStack};
 
 pub const HeapVal = struct {
     address: isize,
     val: isize,
 };
 
+pub const Label = struct {
+    name: isize,
+    pos: isize,
+};
+
 allocator: std.mem.Allocator,
 stack: std.ArrayList(isize),
 heap: std.ArrayList(HeapVal),
-labels: std.ArrayList(isize),
+labels: std.ArrayList(Label),
+call_stack: std.ArrayList(isize),
+chunk: Chunk,
+ip: isize,
 
-pub fn init(allocator: std.mem.Allocator) Self {
+pub fn init(allocator: std.mem.Allocator, chunk: Chunk) Self {
     return Self{
         .allocator = allocator,
         .stack = std.ArrayList(isize).init(allocator),
         .heap = std.ArrayList(HeapVal).init(allocator),
-        .labels = std.ArrayList(isize).init(allocator),
+        .labels = std.ArrayList(Label).init(allocator),
+        .call_stack = std.ArrayList(isize).init(allocator),
+        .chunk = chunk,
+        .ip = 0,
     };
 }
 
@@ -28,39 +44,41 @@ pub fn deinit(self: *Self) void {
     self.stack.deinit();
     self.heap.deinit();
     self.labels.deinit();
+    self.call_stack.deinit();
+    self.ip = 0;
 }
 
 ///////////////////
 // Stack funcs
 ///////////////////
 
-pub fn push(self: *Self, val: isize) std.mem.Allocator.Error!void {
+fn push(self: *Self, val: isize) std.mem.Allocator.Error!void {
     if (self.stack.items.len + 1 > self.stack.capacity) {
         try self.stack.ensureTotalCapacity(self.stack.capacity * 2);
     }
     self.stack.appendAssumeCapacity(val);
 }
 
-pub fn pop(self: *Self) StackErrors!isize {
+fn pop(self: *Self) StackErrors!isize {
     return self.stack.popOrNull() orelse return StackErrors.InsufficientElements;
 }
 
-pub fn resetStack(self: *Self) void {
+fn resetStack(self: *Self) void {
     self.stack.clearRetainingCapacity();
 }
 
-pub fn dup(self: *Self) StackErrors!void {
+fn dup(self: *Self) StackErrors!void {
     const top = self.stack.getLastOrNull() orelse return error{StackEmpty};
     return self.push(top);
 }
 
-pub fn copynth(self: *Self, n: usize) StackErrors!void {
+fn copynth(self: *Self, n: usize) StackErrors!void {
     if (self.stack.items.len < n) return StackErrors.InsufficientElements;
     const val = self.stack.items[n];
     return self.push(val);
 }
 
-pub fn swap(self: *Self) StackErrors!void {
+fn swap(self: *Self) StackErrors!void {
     const items = self.stack.items;
     if (items.len < 2) return StackErrors.InsufficientElements;
     const temp: isize = items[items.len - 1];
@@ -68,7 +86,7 @@ pub fn swap(self: *Self) StackErrors!void {
     items[items.len - 2] = temp;
 }
 
-pub fn arithmetic(
+fn arithmetic(
     self: *Self,
     comptime op: enum { add, sub, mult, div, mod },
 ) StackErrors!void {
@@ -88,7 +106,7 @@ pub fn arithmetic(
 // Heap funcs
 ///////////////////
 
-pub fn store(self: *Self) StackErrors!void {
+fn store(self: *Self) StackErrors!void {
     const val = try self.pop();
     const address = try self.pop();
     for (0.., self.heap.items) |i, item| {
@@ -100,7 +118,7 @@ pub fn store(self: *Self) StackErrors!void {
     try self.heap.append(.{ .address = address, .val = val });
 }
 
-pub fn retrieve(self: *Self) (StackErrors || HeapError)!void {
+fn retrieve(self: *Self) (StackErrors || HeapError)!void {
     const address = try self.pop();
     for (self.heap.items) |v| {
         if (v.address == address) {
@@ -114,28 +132,66 @@ pub fn retrieve(self: *Self) (StackErrors || HeapError)!void {
 // Flow control
 ///////////////////
 
-pub fn reglabel(self: *Self) void {
-    _ = self;
+fn reglabel(self: *Self, label: isize) AllocError!void {
+    try self.labels.append(.{ .name = label, .pos = self.ip + 1 });
+}
+
+fn call(self: *Self, label: isize) LabelErrors!void {
+    for (self.labels.items) |item| {
+        if (item.name == label) {
+            try self.call_stack.append(self.ip + 1);
+            self.ip = item.pos;
+            return;
+        }
+    }
+    return LabelErrors.InvalidLabel;
+}
+
+fn jmp(
+    self: *Self,
+    when: enum { unconditional, iftop0, iftopneg },
+    label: isize,
+) (StackErrors || LabelErrors)!void {
+    for (self.labels.items) |item| {
+        if (item.name == label) {
+            switch (when) {
+                .unconditional => self.ip = item.pos,
+                .iftop0 => {
+                    const top = try self.pop();
+                    if (top == 0) self.ip = item.pos;
+                },
+                .iftopneg => {
+                    const top = try self.pop();
+                    if (top < 0) self.ip = item.pos;
+                },
+            }
+        }
+    }
+}
+
+fn end_subroutine(self: *Self) CallError!void {
+    const tojmp = self.call_stack.popOrNull() orelse return CallError.EmptyCallStack;
+    self.ip = tojmp;
 }
 
 ///////////////////
 // I/O
 ///////////////////
 
-pub fn outc(self: *Self, stdout: std.fs.File.Writer) StackErrors!void {
+fn outc(self: *Self, stdout: std.fs.File.Writer) StackErrors!void {
     stdout.print("{c}", .{try self.pop()});
 }
 
-pub fn outnum(self: *Self, stdout: std.fs.File.Writer) StackErrors!void {
+fn outnum(self: *Self, stdout: std.fs.File.Writer) StackErrors!void {
     stdout.print("{d}", .{try self.pop()});
 }
 
-pub fn readc(self: *Self, stdin: std.fs.File.Reader) !void {
+fn readc(self: *Self, stdin: std.fs.File.Reader) !void {
     const val = try stdin.readByte();
     return self.push(@intCast(val));
 }
 
-pub fn readnum(self: *Self, stdin: std.fs.File.Reader) !void {
+fn readnum(self: *Self, stdin: std.fs.File.Reader) !void {
     const val = try stdin.readInt(isize, .little);
     return self.push(@intCast(val));
 }
